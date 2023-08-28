9bacd49c50aad936ee8deb81975871dbb07ab528
Eric Belongea <eric.belongea@gmail.com>
2023-08-28 07:35:00 -0600

    Adds iteration one

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,15 @@
 # Iterate through the numbers 1 to 1000
 # For each one, print the output according to the rules of Super Fizz Buzz
+numbers =*(1..1000)
+
+numbers.each do |num|
+    if num % 3 == 0
+        p "Fizz"
+    elsif num % 5 == 0
+        p "Buzz"
+    elsif num % 7 == 0
+        p "Super"
+    else
+        p num
+    end
+end
---
+++ spec/sfb_spec.rb
@@ -0,0 +1,8 @@
+require './lib/super_fizz_buzz'
+
+# RSpec.describe "SFB" do
+#     it 'has the array' do 
+#         numbers =*(1..1000)
+#         expect(numbers.count).to eq(1000)
+#     end
+# end
