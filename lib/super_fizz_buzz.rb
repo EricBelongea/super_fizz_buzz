f6c541901a59c77ffa72024baecc7104741f2093
Eric Belongea <eric.belongea@gmail.com>
2023-08-28 09:00:42 -0600

    Updates order of operations

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -3,7 +3,15 @@
 numbers =*(1..1000)
 
 numbers.each do |num|
-    if num % 3 == 0
+    if num % 3 == 0 and num % 5 ==0 and num % 7 == 0
+        p "SuperFizzBuzz"
+    elsif num % 3 == 0 and num % 7 == 0
+        p "SuperFizz"
+    elsif num % 5 == 0 and num % 7 == 0
+        p "SuperBuzz"
+    elsif num % 3 == 0 and num % 5 == 0
+        p "FizzBuzz"
+    elsif num % 3 == 0
         p "Fizz"
     elsif num % 5 == 0
         p "Buzz"
